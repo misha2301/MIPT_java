d6721bbc8494f36d205eee4f4f9b418708834797
misha2301 <mishalosik@gmail.com>
2025-05-03 20:56:28 +0500

    Третья домашка

---
+++ src/main/java/edu/phystech/hw3/result/Result.java
@@ -0,0 +1,19 @@
+package edu.phystech.hw3.result;
+
+import java.util.function.Function;
+
+public sealed interface Result<T> permits Success, Failure {
+    boolean isFailure();
+
+    boolean isSuccess();
+
+    T getOrDefault(T defaultValue);
+
+    Throwable getExceptionOrNull();
+
+    <R> Result<R> map(Function<T, R> transform);
+
+    T value();
+
+    Throwable e();
+}
---
+++ src/test/java/edu/phystech/hw3/InverseMapKeyValuesTest.java
@@ -0,0 +1,52 @@
+package edu.phystech.hw3;
+
+import java.util.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class InverseMapKeyValuesTest {
+
+    public static <K, V> Map<V, Collection<K>> inverse(Map<? extends K, ? extends V> map) {
+        Map<V, Collection<K>> newMap = new HashMap<>();
+        map.forEach((key, value) -> {
+            newMap.computeIfAbsent(value, v -> new ArrayList<>()).add(key);  // если нет ключа value, то создаем там новый список
+        });
+        return newMap;
+    }
+
+    @Test
+    void noDuplicateValuesTest() {
+        Map<Integer, String> originalMap = Map.of(1, "one", 2, "two");
+        Map<String, Collection<Integer>> expectedMap = Map.of("one", List.of(1), "two", List.of(2));
+
+        Map<String, Collection<Integer>> actualMap = inverse(originalMap);
+
+        Assertions.assertEquals(expectedMap.size(), actualMap.size());
+
+        expectedMap.forEach((key, value) -> {
+            var entry = actualMap.get(key);
+            Assertions.assertNotNull(entry);
+            Assertions.assertEquals(new HashSet<>(value), new HashSet<>(entry));
+        });
+
+    }
+
+    @Test
+    void duplicateValuesTest() {
+        Map<Integer, String> originalMap = Map.of(1, "one", 2, "two", 3, "two");
+        Map<String, Collection<Integer>> expectedMap = Map.of("one", List.of(1), "two", List.of(2, 3));
+
+        Map<String, Collection<Integer>> actualMap = inverse(originalMap);
+
+        Assertions.assertEquals(expectedMap.size(), actualMap.size());
+
+        expectedMap.forEach((key, value) -> {
+            var entry = actualMap.get(key);
+            Assertions.assertNotNull(entry);
+            Assertions.assertEquals(new HashSet<>(value), new HashSet<>(entry));
+        });
+
+    }
+}
+
